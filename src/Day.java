eea9e14389efd6764c01417a28989516e8919bfa
Dave <dew1g18@soton.ac.uk>
2020-02-08 15:26:10 +0000

    Interface created and a couple starting objects formed

---
+++ src/Day.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class Day {
+
+    /**
+     * Task and interval number (which interval you're on)
+     * This is going to have tasks where the name for each repeated (continued task) is
+     * mutated to reflect its interval.
+     */
+    private Tuple<Task, Integer> taskIntervals;
+
+
+
+
+}
